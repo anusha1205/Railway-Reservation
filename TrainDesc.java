c696264de543421766a468ad5890582a4eee1327
Anusha Gonal <113822918+anusha1205@users.noreply.github.com>
2023-10-12 21:56:26 +0530

    Delete TrainDesc.java

--- TrainDesc.java
+++
@@ -1,9 +0,0 @@
-package Model;
-public interface TrainDesc {
-    void addTrainObjectives(String objective);
-    void removeTrainObjectives(int objective_no);
-    void displayTrainObjectives();
-    void addTrainOutcomes(String outcome);
-    void removeTrainOutcomes(int outcome_no);
-    void displayTrainOutcomes();
-}
