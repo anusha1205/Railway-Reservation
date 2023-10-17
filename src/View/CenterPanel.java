8e381f5519e7a429e5025de20e11c52e68acf249
Anusha Gonal <113822918+anusha1205@users.noreply.github.com>
2023-10-17 18:06:42 +0530

    Update CenterPanel.java

--- src/View/CenterPanel.java
+++ src/View/CenterPanel.java
@@ -1,85 +1,6 @@
-//package View;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.util.ArrayList;
-//
-//public class CenterPanel extends JPanel {
-//
-//    ArrayList<JButton> header_buttons = new ArrayList<>();
-//    ArrayList<JButton> student_buttons = new ArrayList<>();
-//
-//    public CenterPanel()
-//    {
-//        super();
-//    }
-//
-//    public void createDisplay(int rows, int cols)
-//    {
-//        this.setLayout(new GridLayout(rows+1,cols));
-//        for (int i = 1; i <= cols; i++)
-//        {
-//            JButton b = new JButton();
-//            b.setBackground(Color.lightGray);
-//            b.setPreferredSize(new Dimension(80,25));
-//            header_buttons.add(b);
-//            this.add(b);
-//            validate();
-//            repaint();
-//        }
-//        for (int i = 1; i <= rows * cols; i++)
-//        {
-//            JButton b = new JButton();
-//            b.setBackground(Color.cyan);
-//            b.setPreferredSize(new Dimension(120,25));
-//            student_buttons.add(b);
-//            this.add(b);
-//            validate();
-//            repaint();
-//        }
-//
-//    }
-//
-//    public void updateDisplay(ArrayList<ArrayList<String>> lines, ArrayList<String> headers) {
-//        for (int i = 0; i < headers.size(); i++)
-//        {
-//            setHeaderButtonText(i,headers.get(i));
-//        }
-//
-//        for (int player_no = 0; player_no < lines.size(); player_no++)
-//        {
-//            for (int player_col_no = 0; player_col_no < headers.size(); player_col_no++)
-//            {
-//                int button_no = player_no * headers.size() + player_col_no;
-//                String button_txt = lines.get(player_no).get(player_col_no);
-//                setPlayerButtonText(button_no,button_txt);
-//            }
-//        }
-//    }
-//
-//    public void setHeaderButtonText(int button_no, String button_text) {
-//        header_buttons.get(button_no).setText(button_text);
-//    }
-//    public void setPlayerButtonText(int button_no, String button_text) {
-//        student_buttons.get(button_no).setText(button_text);
-//    }
-//
-//    public ArrayList<JButton> getHeaderButtons()
-//    {
-//        return header_buttons;
-//    }
-//
-//    public ArrayList<JButton> getPlayerButtons()
-//    {
-//        return student_buttons;
-//    }
-//
-//}
-
 package View;
 import Model.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -87,9 +8,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-
 public class CenterPanel extends JPanel {
-
     ArrayList<JButton> header_buttons = new ArrayList<>();
     ArrayList<JButton> passenger_buttons = new ArrayList<>();
     ArrayList<String> fieldValues = new ArrayList<>();
@@ -101,34 +20,36 @@ public class CenterPanel extends JPanel {
 
         // Create the heading label
         JLabel headingLabel = new JLabel("Welcome To Railway Reservation !!!");
-        headingLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        headingLabel.setFont(new Font("SanSerif", Font.ITALIC, 25));
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 3; // Span 3 columns
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.CENTER;
         gbc.insets = new Insets(10, 10, 10, 10); // Padding
         add(headingLabel, gbc);
 
-        // Create a panel for the musician-related buttons
+        // Create a panel for the Passenger-related buttons
         final JPanel passengerPanel = new JPanel(new GridLayout(1, 3));
         passengerPanel.setBorder(BorderFactory.createTitledBorder("Passengers"));
         String[] addPassengerTextValues = {"NAME", "EMAIL","ADDRESS" , "AADHAR NUMBER"};
         JComponent addPassengerButtonPanel = createStyledButton("Add a Passenger", addPassengerTextValues);
-        String[] deletePassengerTextValues = {"DELETE PASSENGER OF ID"};
+        String[] deletePassengerTextValues = {"ENTER PASSENGER ID"};
         JComponent deletePassengerButtonPanel = createStyledButton("Delete a Passenger",deletePassengerTextValues);
         String[] editPassengerTextValues = {"ID","NAME", "EMAIL", "ADDRESS", "AADHAAR NUMBER"};
         JComponent editPassengerButtonPanel = createStyledButton("Edit a Passenger",editPassengerTextValues);
         String[] addTrainTextValues = {"TRAIN NAME", "DEPARTURE STATION", "ARRIVAL DESTINATION", "COACH"};
-        JComponent addTrainButtonPanel = createStyledButton("Choose (Add) a Train",addTrainTextValues);
-        String[] deleteTrainTextValues = {"ID"};
+        JComponent addTrainButtonPanel = createStyledButton("Add a Train",addTrainTextValues);
+        String[] deleteTrainTextValues = {"ENTER ID OF TRAIN"};
         JComponent deleteTrainButtonPanel = createStyledButton("Delete a Train",deleteTrainTextValues);
-        String[] editTrainTextValues = {"NAME", "GENRE", "FORMATION DATE", "AWARDS WON"};
-        JComponent editTrainButtonPanel = createStyledButton("Edit a Train",editTrainTextValues);
-        String[] addBookingTextValues = {"PASSENGER ID", "TRAIN ID", "DATE"};
+//        String[] editTrainTextValues = {"NAME", "DEPARTURE", "ARRIVAL", "COACH"};
+//        JComponent editTrainButtonPanel = createStyledButton("Edit a Train",editTrainTextValues);
+        String[] addBookingTextValues = {"PASSENGER ID", "TRAIN ID"};
         JComponent addBookingButtonPanel = createStyledButton("Add a Booking",addBookingTextValues);
-        String[] deleteBookingTextValues = {"ID","PASSENGER ID","DATE ID"};
-        JComponent deleteBookingButtonPanel = createStyledButton("Display a Booking",deleteBookingTextValues);
-        String[] display = {};
+        String[] displayBookingTextValues = {"PASSENGER ID","TRAIN ID"};
+        JComponent displayBookingButtonPanel = createStyledButton("Display a Booking",displayBookingTextValues);
+        String[] display = {
+
+        };
 
 
 
@@ -148,20 +69,20 @@ public class CenterPanel extends JPanel {
 
         middlePanel.add(addTrainButtonPanel);
         middlePanel.add(deleteTrainButtonPanel);
-        middlePanel.add(editTrainButtonPanel);
+//        middlePanel.add(editTrainButtonPanel);
 
         gbc.gridy = 2; // Move to the next row
         add(middlePanel, gbc);
 
         // Create the bottom division with enrollment-related buttons
         JPanel bottomPanel = new JPanel(new GridLayout(1, 3));
-        bottomPanel.setBorder(BorderFactory.createTitledBorder("Enrollments"));
+        bottomPanel.setBorder(BorderFactory.createTitledBorder("BOOKINGS"));
 
         JPanel displayPanel = new JPanel(new GridLayout(1,2));
-        displayPanel.setBorder(BorderFactory.createTitledBorder("Display"));
+        displayPanel.setBorder(BorderFactory.createTitledBorder("DISPLAY"));
 
         bottomPanel.add(addBookingButtonPanel);
-        bottomPanel.add(deleteBookingButtonPanel);
+        bottomPanel.add(displayBookingButtonPanel);
 
         gbc.gridy = 3; // Move to the next row
         add(bottomPanel, gbc);
@@ -173,8 +94,8 @@ public class CenterPanel extends JPanel {
         JPanel panel = new JPanel(new GridBagLayout());
 
         JButton button = new JButton(text);
-        button.setFont(new Font("Arial", Font.PLAIN, 16));
-        button.setForeground(Color.ORANGE); // Orange text color
+        button.setFont(new Font("SanSerif", Font.BOLD, 16));
+        button.setForeground(Color.BLACK); // Orange text color
         button.setFocusPainted(false); // Remove focus border
 
         GridBagConstraints buttonConstraints = new GridBagConstraints();
@@ -219,9 +140,10 @@ public class CenterPanel extends JPanel {
                     JTextField emailTextField = textFields.get(1); String email = emailTextField.getText();
                     JTextField addressTextField = textFields.get(2); String address = addressTextField.getText();
                     JTextField aadhar_numberTextField = textFields.get(3); long aadhar_number = Long.parseLong(aadhar_numberTextField.getText());
+                    id+=1;
                     Passenger pass = new Passenger(id,name,email,address,aadhar_number);
                     passengers.add(pass);
-                    JOptionPane.showMessageDialog(panel, "Passenger (Created) Registered", "DONE", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(panel, "Passenger Registered Successfully !", "DONE", JOptionPane.INFORMATION_MESSAGE);
                     try {
                         mapper.writeValue(Paths.get("src/Model/passengers.json").toFile(), passengers);
                     } catch (IOException ex) {
@@ -248,7 +170,7 @@ public class CenterPanel extends JPanel {
                     passengers.add(pass);
                     JOptionPane.showMessageDialog(panel, "Passenger Updated", "DONE", JOptionPane.INFORMATION_MESSAGE);
                     try {
-                        mapper.writeValue(Paths.get("src/Model/musician.json").toFile(), passengers);
+                        mapper.writeValue(Paths.get("src/Model/passengers.json").toFile(), passengers);
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
@@ -263,10 +185,9 @@ public class CenterPanel extends JPanel {
                     managePassenger mm1 = new managePassenger();
                     passengers = mm1.readJsonFile("src/Model/passengers.json");
                     ObjectMapper mapper = new ObjectMapper();
-
                     JTextField idTextField = textFields.get(0); int id = Integer.parseInt(idTextField.getText());
                     passengers.remove(id-1);
-                    JOptionPane.showMessageDialog(panel, "Passenger Deleted", "DONE", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(panel, "Passenger Deleted Successfully !", "DONE", JOptionPane.INFORMATION_MESSAGE);
                     try {
                         mapper.writeValue(Paths.get("src/Model/passengers.json").toFile(), passengers);
                     } catch (IOException ex) {
@@ -275,7 +196,7 @@ public class CenterPanel extends JPanel {
                 }
             });
         }
-        else if (text.equals("Choose (Add) a Train")) {
+        else if (text.equals("Add a Train")) {
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -287,14 +208,11 @@ public class CenterPanel extends JPanel {
 
                     JTextField train_nameTextField = textFields.get(0);
                     String train_name = train_nameTextField.getText();
-
                     JTextField train_departTextField = textFields.get(1);
                     String train_depart = train_departTextField.getText();
-
                     JTextField train_arrivalTextField = textFields.get(2);
                     String train_arrival = train_arrivalTextField.getText();
-
-                    JTextField train_coachTextField = textFields.get(2);
+                    JTextField train_coachTextField = textFields.get(3);
                     String train_coach = train_coachTextField.getText();
                     Train bd = new Train(trainId, train_name, train_depart, train_arrival, train_coach);
                     trains.add(bd);
@@ -307,31 +225,41 @@ public class CenterPanel extends JPanel {
                 }
             });
         }
-        else if (text.equals("Edit a Train")) {
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    ArrayList<Train> trains = new ArrayList<Train>();
-                    manageTrain mb1 = new manageTrain();
-                    trains = mb1.readJsonFile("src/Model/train.json");
-                    ObjectMapper mapper = new ObjectMapper();
-
-                    JTextField trainIdTextField = textFields.get(0); int trainId = Integer.parseInt(trainIdTextField.getText());
-                    JTextField trainNameTextField = textFields.get(1); String train_name = trainNameTextField.getText();trains.get(trainId-1).setTrainName(train_name);
-                    JTextField trainDepartTextField = textFields.get(2); String train_depart = trainDepartTextField.getText();trains.get(trainId-1).setTrainDepart(train_depart);
-                    JTextField trainArrivalTextField = textFields.get(3); String train_arrival = trainArrivalTextField.getText();trains.get(trainId-1).setTrainArrial(train_arrival);
-                    JTextField trainCoachTextField = textFields.get(3); String train_coach = trainCoachTextField.getText();trains.get(trainId-1).setTrainCoach(train_coach);
-                    Train bd = new Train(trainId,train_name,train_depart,train_arrival,train_coach);
-                    trains.add(bd);
-                    JOptionPane.showMessageDialog(panel, "Train Updated", "DONE", JOptionPane.INFORMATION_MESSAGE);
-                    try {
-                        mapper.writeValue(Paths.get("src/Model/train.json").toFile(),trains);
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-            });
-        }
+//        else if (text.equals("Edit a Train")) {
+//            button.addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//                    ArrayList<Train> trains = new ArrayList<Train>();
+//                    manageTrain mb1 = new manageTrain();
+//                    trains = mb1.readJsonFile("src/Model/train.json");
+//                    ObjectMapper mapper = new ObjectMapper();
+//
+//                    JTextField train_idTextField = textFields.get(0);
+//                    int train_id = Integer.parseInt(train_idTextField.getText());
+//                    JTextField trainNameTextField = textFields.get(1);
+//                    String train_name = trainNameTextField.getText();
+//                    trains.get(train_id-1).setTrainName(train_name);
+//                    JTextField trainDepartTextField = textFields.get(2);
+//                    String train_depart = trainDepartTextField.getText();
+//                    trains.get(train_id-1).setTrainDepart(train_depart);
+//                    JTextField trainArrivalTextField = textFields.get(3);
+//                    String train_arrival = trainArrivalTextField.getText();
+//                    trains.get(train_id-1).setTrainArrial(train_arrival);
+//                    JTextField trainCoachTextField = textFields.get(3);
+//                    String train_coach = trainCoachTextField.getText();
+//                    trains.get(train_id-1).setTrainCoach(train_coach);
+//
+//                    Train bd = new Train(train_id,train_name,train_depart,train_arrival,train_coach);
+//                    trains.add(bd);
+//                    JOptionPane.showMessageDialog(panel, "Train Updated", "DONE", JOptionPane.INFORMATION_MESSAGE);
+//                    try {
+//                        mapper.writeValue(Paths.get("src/Model/train.json").toFile(),trains);
+//                    } catch (IOException ex) {
+//                        throw new RuntimeException(ex);
+//                    }
+//                }
+//            });
+//        }
         else if (text.equals("Delete a Train")){
             button.addActionListener(new ActionListener() {
                 @Override
@@ -341,9 +269,10 @@ public class CenterPanel extends JPanel {
                     trains = mb1.readJsonFile("src/Model/train.json");
                     ObjectMapper mapper = new ObjectMapper();
 
-                    JTextField trainIdTextField = textFields.get(0);int trainId = Integer.parseInt(trainIdTextField.getText());
+                    JTextField trainIdTextField = textFields.get(0);
+                    int trainId = Integer.parseInt(trainIdTextField.getText());
                     trains.remove(trainId-1);
-                    JOptionPane.showMessageDialog(panel, "Train Deleted", "DONE", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(panel, "Train Deleted Successfully !", "DONE", JOptionPane.INFORMATION_MESSAGE);
                     try {
                         mapper.writeValue(Paths.get("src/Model/train.json").toFile(),trains);
                     } catch (IOException ex) {
@@ -370,19 +299,37 @@ public class CenterPanel extends JPanel {
                     JTextField bidTextField = textFields.get(0);
                     int bid = Integer.parseInt(bidTextField.getText());
 
-                    JTextField dojTextField = textFields.get(0);
-                    String doj = dojTextField.getText();
+//                    JTextField dojTextField = textFields.get(0);
+//                    String doj = dojTextField.getText();
 
                     book_array[count] = new Booking();
                     count++;
-                    book_array[count-1].booking_pass_train(pass_array[mid-1],train_array[bid-1],doj);
+//                    book_array[count-1].display_booked_passenger_train();
+                    book_array[count-1].booking_pass_train(pass_array[mid-1],train_array[bid-1]);
 
                     JOptionPane.showMessageDialog(panel, "Booking Added !", "DONE", JOptionPane.INFORMATION_MESSAGE);
 
                 }
             });
         }
-    return panel;
+        else if (text.equals("Display a Booking")) {
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ArrayList<Passenger> passengers = new ArrayList<Passenger>();
+                    managePassenger mm1 = new managePassenger();
+                    passengers = mm1.readJsonFile("src/Model/passengers.json");
+                    mm1.writeJsonFile(passengers);
+                    ArrayList<String> passengerss = mm1.getHeaders();
+                    //  customers = mm1.getLine();
+                    ArrayList<ArrayList<String>> passengersss = mm1.getLines(1,15);
+                    passengerss = mm1.getTable();
+
+                    JOptionPane.showMessageDialog(panel, "PASSENGER BOOKED SUCCESSFULLY", "DONE", JOptionPane.INFORMATION_MESSAGE);
+                }
+            });
+        }
+        return panel;
     }
 
     public void createDisplay ( int rows, int cols)
@@ -434,7 +381,6 @@ public class CenterPanel extends JPanel {
     {
         return header_buttons;
     }
-
     public ArrayList<JButton> getPlayerButtons ()
     {
         return passenger_buttons;
@@ -443,6 +389,5 @@ public class CenterPanel extends JPanel {
         return fieldValues;
     }
     public void verify(){
-
     }
 }
