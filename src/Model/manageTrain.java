09344e7438d9cf8169999bbfcaec4c45f1ec3e44
Anusha Gonal <113822918+anusha1205@users.noreply.github.com>
2023-10-17 18:08:22 +0530

    Update manageTrain.java

--- src/Model/manageTrain.java
+++ src/Model/manageTrain.java
@@ -1,14 +1,12 @@
 package Model;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 public class manageTrain {
     ArrayList<Train> trains = new ArrayList<Train>();
     private Object String;
-
     public ArrayList<Train> loadJsonFile(String file_path) {
         return readJsonFile(file_path);
     }
@@ -22,17 +20,17 @@ public class manageTrain {
             // Iterate through JSON array
             if (rootNode.isArray()) {
                 for (JsonNode node : rootNode) {
-                    int trainId = node.get("trainId").asInt();
-                    String trainName = node.get("trainName").asText();
-                    String trainDepart = node.get("trainDepart").asText();
-                    String trainArrival = node.get("trainArrival").asText();
-                    String trainCoach = node.get("trainCoach").asText();
-                    Train temp_train = new Train(trainId,trainName,trainDepart,trainArrival,trainCoach);
-                    temp_train.setTrainId(trainId);
-                    temp_train.setTrainName(trainName);
-                    temp_train.setTrainDepart(trainDepart);
-                    temp_train.setTrainArrial(trainArrival);
-                    temp_train.setTrainCoach(trainCoach);
+                    int train_id = node.get("train_id").asInt();
+                    String train_name = node.get("train_name").asText();
+                    String train_depart = node.get("train_depart").asText();
+                    String train_arrival = node.get("train_arrival").asText();
+                    String train_coach = node.get("train_coach").asText();
+                    Train temp_train = new Train(train_id,train_name,train_depart,train_arrival,train_coach);
+                    temp_train.setTrain_id(train_id);
+                    temp_train.setTrain_name(train_name);
+                    temp_train.setTrain_depart(train_depart);
+                    temp_train.setTrain_arrival(train_arrival);
+                    temp_train.setTrain_coach(train_coach);
                     trains.add(temp_train);
                 }
             }
@@ -43,3 +41,4 @@ public class manageTrain {
         return trains;
     }
 }
+
